5bb546ce37e8061345d590f254cf5a18ca9a4465
Sabya Roy <68545445+mr-sabya@users.noreply.github.com>
2026-04-12 19:34:28 +0600

    fix: Update doctor schedule management to reference the correct schedule ID

--- resources/views/admin/doctor-schedule/edit.blade.php
+++ resources/views/admin/doctor-schedule/edit.blade.php
@@ -5,5 +5,5 @@
 <!-- page title -->
 <livewire:admin.theme.page-title :title="'Edit Doctor Schedule'" :breadcrumb="'Edit Doctor Schedule'" />
 
-<livewire:admin.doctor-schedule.manage id="{{ $patient->id }}" />
+<livewire:admin.doctor-schedule.manage id="{{ $schedule->id }}" />
 @endsection
